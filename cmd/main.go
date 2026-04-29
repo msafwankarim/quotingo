c56eaa5c42873968454a8d6541109666ee71dbe4
Safwan <msafwankarim786@gmail.com>
2026-04-30 01:19:56 +0500

    update version to alpha-02

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,7 @@ type Author struct {
 }
 
 var (
-	version = "1.0.0-alpha-01"
+	version = "1.0.0-alpha-02"
 	authors = []Author{
 		{Name: "Muhammad Safwan Karim", RegNo: "537263"},
 		{Name: "Muhammad Jahanzeb", RegNo: "537531"},
