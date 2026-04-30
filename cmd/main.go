68b394334fc989e26fd755ead138f1efa4679deb
Safwan <msafwankarim786@gmail.com>
2026-04-30 14:36:55 +0500

    update version to alpha-02

--- cmd/main.go
+++ cmd/main.go
@@ -19,9 +19,9 @@ var (
 	authors = []Author{
 		{Name: "Muhammad Safwan Karim", RegNo: "537263"},
 		{Name: "Muhammad Jahanzeb", RegNo: "537531"},
-		{Name: "Malik Muhammad Mueed Aslam", RegNo: "538108"},
+		{Name: "Malik Muhammad Mueed Aslam", RegNo: "538285"},
 		{Name: "Waseem Gul", RegNo: "536658"},
-		{Name: "Muhammad Muneeb Baig", RegNo: "538655"},
+		{Name: "Muhammad Muneeb Baig", RegNo: "536855"},
 	}
 
 	jokeCache = &repository.JokeQueue{}
